9e1a4ba57cb5c228964332e1ea773732a4db24fa
DeLxCruz <lopezsantiago3503@gmail.com>
2024-05-19 21:52:22 -0500

    feat: :recycle: Format code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,44 +13,101 @@ const Header = () => {
             Mejora tu calidad de vida con tecnología inteligente
           </p>
         </div>
-        <img className="md:h-full md:w-[700px] rounded-2xl" src="../../public/img/mujer-joven-tecnologia-casera.jpg" alt="" />
+        <img
+          className="md:h-full md:w-[700px] rounded-2xl"
+          src="../../public/img/mujer-joven-tecnologia-casera.jpg"
+          alt=""
+        />
       </section>
-    <section className="mt-16 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+      <section className="mt-16 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
           <li>
-            <img width={130} height={40} src="../../public/img/google-2015-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/amazon-echo-logo-svgrepo-com.svg" alt=""/>
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/samsung-1-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/lg-electronics-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/xiaomi-logo-svgrepo-com.svg" alt="" />
-            </li>
-          </ul>
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
-            <li>
-            <img width={130} height={40} src="../../public/img/google-2015-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/amazon-echo-logo-svgrepo-com.svg" alt=""/>
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/samsung-1-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/lg-electronics-logo-svgrepo-com.svg" alt="" />
-            </li>
-            <li>
-            <img width={130} height={40} src="../../public/img/xiaomi-logo-svgrepo-com.svg" alt="" />
-            </li>
-          </ul>
-        </section>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/google-2015-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/amazon-echo-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/samsung-1-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/lg-electronics-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/xiaomi-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/google-2015-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/amazon-echo-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/samsung-1-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/lg-electronics-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+          <li>
+            <img
+              width={130}
+              height={40}
+              src="../../public/img/xiaomi-logo-svgrepo-com.svg"
+              alt=""
+            />
+          </li>
+        </ul>
+      </section>
     </header>
   );
 };
