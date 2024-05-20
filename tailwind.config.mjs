f075d958ddbf2e1c812cafbcba6894f4e9a21d57
DeLxCruz <lopezsantiago3503@gmail.com>
2024-05-20 10:54:19 -0500

    feat: :sparkles: Introduce Sticky Scroll Reveal Section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       animation: {
-        "infinite-scroll": "infinite-scroll 15s linear infinite",
+        "infinite-scroll": "infinite-scroll 20s linear infinite",
       },
       keyframes: {
         "infinite-scroll": {
@@ -14,5 +21,17 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
